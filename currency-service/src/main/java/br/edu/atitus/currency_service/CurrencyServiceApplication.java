7da437d36031c08831abaa7cb448e49cfe37f17c
Júlia Wonsick <1136562@atitus.edu.br>
2025-05-21 20:37:49 -0300

    Adicionado conexão da API externa ao Banco Central do Brasil

--- currency-service/src/main/java/br/edu/atitus/currency_service/CurrencyServiceApplication.java
+++ currency-service/src/main/java/br/edu/atitus/currency_service/CurrencyServiceApplication.java
@@ -2,8 +2,11 @@ package br.edu.atitus.currency_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignClient;
 
 @SpringBootApplication
+@EnableFeignClients
 public class CurrencyServiceApplication {
 
 	public static void main(String[] args) {
