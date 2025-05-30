fe28526b85fded0a4b8dc360872788a648d4d64b
Júlia Wonsick <1136562@atitus.edu.br>
2025-05-29 23:05:19 -0300

    Adição do Circuit Breaker no Product-Servicee Cache com Caffeine no Product-Service

--- currency-service/src/main/java/br/edu/atitus/currency_service/CurrencyServiceApplication.java
+++ currency-service/src/main/java/br/edu/atitus/currency_service/CurrencyServiceApplication.java
@@ -2,11 +2,13 @@ package br.edu.atitus.currency_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignClient;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableCaching
 public class CurrencyServiceApplication {
 
 	public static void main(String[] args) {
--- product-service/src/main/java/br/edu/atitus/product_service/controller/OpenProductController.java
+++ product-service/src/main/java/br/edu/atitus/product_service/controller/OpenProductController.java
@@ -1,8 +1,7 @@
 package br.edu.atitus.product_service.controller;
 
-
-
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.CacheManager;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,39 +17,57 @@ import br.edu.atitus.product_service.repositories.ProductRepository;
 @RequestMapping("products")
 public class OpenProductController {
 
-    private final ProductRepository repository;
-    private final CurrencyClient currencyClient;
-      
-    @Value("${server.port}")
-    private String port;
+	private final ProductRepository repository;
+	private final CurrencyClient currencyClient;
+	private final CacheManager cacheManager;
 
-    public OpenProductController(ProductRepository repository, CurrencyClient currencyClient) {
-        this.repository = repository;
+	@Value("${server.port}")
+	private String port;
+
+	public OpenProductController(ProductRepository repository, CurrencyClient currencyClient,
+			CacheManager cacheManager) {
+		this.repository = repository;
 		this.currencyClient = currencyClient;
-    }
-    
-    @Value("${server.port}")
-    private int serverPort;
-    
-    @GetMapping("/{idProduct}/{targetCurrency}")
-    public ResponseEntity<ProductEntity> getProduct(
-    		@PathVariable Long idProduct,
-    		@PathVariable String targetCurrency
-    		) throws Exception{
-    	ProductEntity product = repository.findById(idProduct)
-    			.orElseThrow(() -> new Exception("Product not found"));
-    	product.setEnvironment("porduct-service running on Port: " + serverPort);
-    	
-    	if(product.getCurrency().equals(targetCurrency)) {
-    		product.setConvertedPrice(product.getPrice());
-    		
- 
-    } else {
-		CurrencyResponse currency = currencyClient.getCurrency(product.getPrice(),product.getCurrency(),targetCurrency);
-		product.setConvertedPrice(currency.getConvertedValue());
-		product.setEnvironment(product.getEnvironment() + " - " + currency.getEnviroment());
+		this.cacheManager = cacheManager;
 	}
-    
-    	return ResponseEntity.ok(product);
-    }
+
+	@Value("${server.port}")
+	private int serverPort;
+
+	@GetMapping("/{idProduct}/{targetCurrency}")
+	public ResponseEntity<ProductEntity> getProduct(@PathVariable Long idProduct, @PathVariable String targetCurrency)
+			throws Exception {
+
+		targetCurrency = targetCurrency.toUpperCase();
+		String nameCache = "Product";
+		String keyCache = idProduct + targetCurrency;
+
+		ProductEntity product = cacheManager.getCache(nameCache).get(keyCache, ProductEntity.class);
+
+		if (product == null) {
+
+			product = repository.findById(idProduct).orElseThrow(() -> new Exception("Product not found"));
+			product.setEnvironment("product-service running on Port: " + serverPort);
+
+			if (product.getCurrency().equals(targetCurrency)) {
+				product.setConvertedPrice(product.getPrice());
+
+			} else {
+				CurrencyResponse currency = currencyClient.getCurrency(product.getPrice(), product.getCurrency(),
+						targetCurrency);
+				if (currency != null) {
+					product.setConvertedPrice(currency.getConvertedValue());
+					product.setEnvironment(product.getEnvironment() + " - " + currency.getEnviroment());
+					
+					cacheManager.getCache(nameCache).put(keyCache, product);
+				} else {
+					product.setConvertedPrice((double) -1);
+					product.setEnvironment(product.getEnvironment() + " - Currency unavalaible");
+				}
+			}
+		} else {
+			product.setEnvironment("Product-service running on Port: " + serverPort + " - DataSource: cache");
+		}
+		return ResponseEntity.ok(product);
+	}
 }
